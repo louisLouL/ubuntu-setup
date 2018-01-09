0db6b80c3862548f9f6855dcdc83c50e41e23781
Louis <32419791+louisLouL@users.noreply.github.com>
2018-01-09 15:59:51 -0500

    Update first_setup.sh

--- first_setup.sh
+++ first_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # install some utility
 apt-get update
-apt-get install -y libssl-dev libldap2-dev libffi-dev libpq-dev \
+apt-get install -y openssh-server libssl-dev libldap2-dev libffi-dev libpq-dev \
 curl apt-utils apt-transport-https debconf-utils gcc build-essential g++-5 \
 wget default-jdk software-properties-common ca-certificates
 
