1e4762ffcf69774e3d6a0acdba817aea87a8354e
Louis <qijia.lou@nyu.edu>
2018-03-26 07:57:51 -0400

    add zsh

--- first_setup.sh
+++ first_setup.sh
@@ -3,7 +3,8 @@
 apt-get update
 apt-get install -y openssh-server libssl-dev libldap2-dev libffi-dev libpq-dev \
 curl apt-utils apt-transport-https debconf-utils gcc build-essential g++-5 \
-wget default-jdk software-properties-common ca-certificates
+wget default-jdk software-properties-common ca-certificates zsh
+
 
 # install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
@@ -14,6 +15,15 @@ add-apt-repository \
 apt-get update
 apt-get install -y docker-ce
 usermod -aG docker $(whoami)
+
+su $whoami
+
+# install oh-my-zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# Auto suggestion
+
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
 # config vim
 wget -O ~/.vimrc https://github.com/amix/vimrc/raw/master/vimrcs/basic.vim
