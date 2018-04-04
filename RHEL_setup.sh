ecbb306559017315bc7ecdc7065400b2ef11637a
Louis <32419791+louisLouL@users.noreply.github.com>
2018-04-04 15:00:34 -0400

    Update RHEL_setup.sh

--- RHEL_setup.sh
+++ RHEL_setup.sh
@@ -4,6 +4,8 @@
 
 yum install -y yum-utils device-mapper-persistent-data lvm2
 
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
 subscription-manager repos --enable=rhel-7-server-extras-rpms
 
 wget ftp://ftp.icm.edu.pl/vol/rzm6/linux-oracle-repo/OracleLinux/OL7/developer_EPEL/x86_64/pigz-2.3.4-1.el7.x86_64.rpm
