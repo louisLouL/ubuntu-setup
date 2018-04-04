f68436e907f3c80ebb8b16df0bdff7925a6e1384
Louis <32419791+louisLouL@users.noreply.github.com>
2018-04-04 14:56:25 -0400

    Create RHEL_setup.sh

---
+++ RHEL_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Install docker
+
+yum install -y yum-utils device-mapper-persistent-data lvm2
+
+subscription-manager repos --enable=rhel-7-server-extras-rpms
+
+wget ftp://ftp.icm.edu.pl/vol/rzm6/linux-oracle-repo/OracleLinux/OL7/developer_EPEL/x86_64/pigz-2.3.4-1.el7.x86_64.rpm
+
+rpm -i pigz-2.3.4-1.el7.x86_64.rpm
+
+rm pigz-2.3.4-1.el7.x86_64.rpm
+
+yum install -y docker-ce
+
+systemctl enable docker
+
+systemctl start docker
+
+usermod -aG docker $SUDO_USER
